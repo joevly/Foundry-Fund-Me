168df9a4ccbc3477d36705d9b7f884181c65113d
Joev <aetherblue77@gmail.com>
2026-03-28 21:25:48 +0700

    forge fmt

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,7 +6,6 @@ import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-
     function run() external {
         vm.startBroadcast();
         new FundMe();
