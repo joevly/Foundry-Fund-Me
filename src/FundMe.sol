b654e7d04c0dfb75f08836f94d694c16124777d7
Joev <aetherblue77@gmail.com>
2026-03-30 12:41:07 +0700

    Add getOwner view function

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -13,7 +13,7 @@ contract FundMe {
     address[] private s_funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public immutable i_owner;
+    address private immutable i_owner;
     uint256 public constant MINIMUM_USD = 5e18;
     AggregatorV3Interface private s_priceFeed;
 
@@ -68,5 +68,9 @@ contract FundMe {
 
     function getFunder(uint256 index) external view returns (address) {
         return s_funders[index];
+    }
+
+    function getOwner() external view returns (address) {
+        return i_owner;
     }
 }
